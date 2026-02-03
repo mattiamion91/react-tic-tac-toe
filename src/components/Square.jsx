587048ee745ac0ef4ddd057026d8069c0edb1920
Mattia Mion <mattia.mion@gmail.com>
2026-02-03 11:24:28 +0100

    feat: funzione nel componente square che al evento click mi mostra un log

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,13 @@
 export default function Square({ value }) {
-    return(
-        <button className='quadrato'>{ value }</button>
+
+    // funzione che mi ritona un log allévento click
+    function handleClick() {
+        console.log('hai cliccato!!');
+
+    }
+
+    return (
+        <button onClick={handleClick} 
+        className='quadrato'>{value}</button>
     )
 }
