7c7f92c15cd287c2f4f43864edeed9f4dc0e6c20
Mattia Mion <mattia.mion@gmail.com>
2026-02-03 17:12:02 +0100

    feat: square divetna dumb component e tutta la lofica viene eseguita in board e passata a square con le props

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react"
 import Square from "./square"
 
-export default function Board () {
-    
+export default function Board() {
+
+    //imposto varaibile di stato
+    const [squares, setSquares] = useState(Array(9).fill(null)) //crea un array di 9 elementi con value null
+    //const [squares, setSquares] = useState([1,2,3,4,5,6,7,8,9])
+
+    //imposto var di stato che gestisce se nel quadrato cé una x oppure una o (booleana)
+    const [xIsNext, SetXIsnext] = useState(true)
+
+    //funzione che gestisce il click
+    function handleClick(i) {  //'i' funge sa segna posto
+        if (squares[i]) { //se la casella é gua occupata il return blocca la funzione e il codice che viene dopo non é eseguiti
+            
+            return;
+        }
+        const nextSquares = squares.slice(); // con il metodo slice mi creo una copia dell'array orginale
+        if (xIsNext) {
+            nextSquares[i] = 'X'
+        } else {
+            nextSquares[i] = 'O'
+        }
+        //nextSquares[i] = 'X'; // e nella copia alla posizione i esama il valore non sara null ma x 
+        setSquares(nextSquares); //con il setter cambio dinamicamente ma mia var di stato squares
+        SetXIsnext(!xIsNext); //setter mi imposta il contrario dello state boolean della var in questo modo ad ogni click avro sempre prima una 'x' e poi una 'o'
+    }
     return (
-    <>
-      <div className="riga-griglia">
-        <Square value = '1'/>
-        <Square value = '2'/>
-        <Square value = '3'/>
-      </div>
-      <div className="riga-griglia">
-        <Square value = '4'/>
-        <Square value = '5'/>
-        <Square value = '6'/>
-      </div>
-      <div className="riga-griglia">
-        <Square value = '7'/>
-        <Square value = '8'/>
-        <Square value = '9'/>
-      </div>
-    </>
-  )
+        <>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+        </>
+    )
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+//import { useState } from "react";
 //rimuovo value
-export default function Square() {
+export default function Square({value, onSquareClick}) {
 
     //imposto var di stato che deve ricordare che la casella é stata cliccata valore inziale 'null'
-    const [valore, setValore] = useState(null)
+    //const [value, setValue] = useState(null)
 
     // funzione che mi ritona un log allévento click
-    function handleClick() {
-        setValore ('X')//setvalore cambia la var di sato valore in modo dimanico
-        console.log('hai cliccato!!');
+    //function handleClick() {
+    // setValue ('X')//setvalore cambia la var di sato valore in modo dimanico
+    //console.log('hai cliccato!!');
 
-    }
+    //}
 
     return (
-        <button onClick={handleClick} 
-        className='quadrato'>{valore}</button>
+        <button onClick={onSquareClick} //esegue la funzione handleclick passata con le props
+            className='square'>
+            {value}
+        </button>
     )
 }
