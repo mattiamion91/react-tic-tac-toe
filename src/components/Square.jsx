3c46804a44ffed75cfe550420ef864942cf0dfc1
Mattia Mion <mattia.mion@gmail.com>
2026-02-03 11:44:52 +0100

    feat: usestate e var di stato che mi renderizza la x al click

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,19 @@
-export default function Square({ value }) {
+import { useState } from "react";
+//rimuovo value
+export default function Square() {
+
+    //imposto var di stato che deve ricordare che la casella é stata cliccata valore inziale 'null'
+    const [valore, setValore] = useState(null)
 
     // funzione che mi ritona un log allévento click
     function handleClick() {
+        setValore ('X')//setvalore cambia la var di sato valore in modo dimanico
         console.log('hai cliccato!!');
 
     }
 
     return (
         <button onClick={handleClick} 
-        className='quadrato'>{value}</button>
+        className='quadrato'>{valore}</button>
     )
 }
